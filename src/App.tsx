98fce5563ae8c0a34cc9d10dd1ef86d6432f96c3
Kotauror <zygmunt.justyna@gmail.com>
2020-09-24 14:36:47 +0100

    Add map with initial examples.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MapContainer from './MapContainer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         >
          Kościółkowo! 
         </a>
+        <MapContainer />
       </header>
     </div>
   );
