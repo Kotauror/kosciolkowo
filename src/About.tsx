7df9692fa06a620fc12618adba716656ed1e8aa3
Kotauror <zygmunt.justyna@gmail.com>
2020-12-22 15:57:41 +0000

    Changes to about section.

--- src/About.tsx
+++ src/About.tsx
@@ -1,16 +1,34 @@
 import React, { FunctionComponent } from "react";
+import styled from "styled-components";
 
 interface IAbout {
   className?: string;
+  theme?: {
+    colours: {
+    primary: string;
+    }
+  };
 }
 
 export const About: FunctionComponent<IAbout> = ({ className }) => {
   return (
     <div className={className}>
-      OLABOGA
-      KOASCIOLKOWO
+    <div className="aboutWrapper">
+      <h1>O projekcie</h1>
+      <h1>Jak gromadzono dane</h1>
+      </div>
     </div>
   );
 };
 
-export default About;
+const styledAbout = styled(About)`
+.aboutWrapper {
+  margin-top: 10em;
+}
+h1 {
+  text-align: center;
+  color: ${props => props.theme.colours.primary};
+}
+`
+
+export default styledAbout;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 const theme = {
   navbarHeight: "3em",
   colours: {
-    primary: "#3581B8",
+    primary: "#2f7890",
     secondary: "#FCB07E",
     tertiary: "#B24C63",
     lightAlabaster: "#F3F8F2",
